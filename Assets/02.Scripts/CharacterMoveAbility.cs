1b60dbcfc3e9cac7fd27aed117c67a94e9c0e470
조희수 <hs.jo.sw.p@gmail.com>
2024-04-11 21:50:08 +0900

    플레이어 에셋 추가 및 시네머신 카메라, WASD 이동 구현

---
+++ Assets/02.Scripts/CharacterMoveAbility.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+
+public class CharacterMoveAbility : MonoBehaviour
+{
+    // 실습 과제 1. 캐릭터 이동 기능 구현 [WASD] 키
+    CharacterController _characterController;
+    Animator _animator;
+    public float MoveSpeed = 2f;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // 1. 입력받기
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 2. 방향구하기
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        // 3-1. 중력값 적용
+        dir.y = -1f;
+
+        // 3. 이동하기
+        _characterController.Move(dir * MoveSpeed * Time.deltaTime);
+        //_animator.SetFloat("Move");
+    }
+
+}
+
---
+++ Assets/02.Scripts/CharacterRotateAbility.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotateAbility : MonoBehaviour
+{
+    // 실습 과제 2. 캐릭터 및 카메라 회전 기능 구현 (3인칭, 시네머신: Virtual Camera 사용) [마우스] 이동
+
+    private float _mx;
+    private float _my;
+    public float RotationSpeed = 200f;
+    public Transform CameraRoot;
+
+    private void Update()
+    {
+        // 1. 마우스 입력 값
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // 2. 회전 값 누적
+        _mx += mouseX * RotationSpeed * Time.deltaTime;
+        _my += mouseY * RotationSpeed * Time.deltaTime;
+        _my = Mathf.Clamp(_my, -90f, 90f);
+
+        // 3. 캐릭터와 카메라를 회전
+        transform.eulerAngles = new Vector3(0, _mx, 0f);
+        CameraRoot.localEulerAngles = new Vector3(-_my, 0, 0f);
+
+        // 4. 시네머신 -> 패키지매니저에서 설치
+        // Virtual Camera -> body: 3rd Person Follow, rig: offset조절
+
+    }
+}
