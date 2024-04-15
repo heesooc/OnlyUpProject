9db8076fcd824c3ecea19cf2bb01cfd35d33f761
조희수 <hs.jo.sw.p@gmail.com>
2024-04-15 15:35:02 +0900

    침대 점프 구현

--- Assets/02.Scripts/CharacterMoveAbility.cs
+++ Assets/02.Scripts/CharacterMoveAbility.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class CharacterMoveAbility : MonoBehaviour
 {
-    // 점프, 스태미나 구현
-    //public bool IsJumping => !_characterController.isGrounded; //-> true면 물체 잡아
-    // todo. 물체 잡은게 true이면 -> 올라가
-
-    // * 캐릭터 이동 기능 구현 [WASD] 키
     CharacterController _characterController;
     Animator _animator;
 
@@ -18,7 +13,8 @@ public class CharacterMoveAbility : MonoBehaviour
     public float JumpPower = 5f;
     private float _yVelocity = 0f;
     private float _gravity = -9.8f;
-    private float _velocitySmoothing;
+
+    public float enhancedJumpPower = 15f; // 침대에서 뛸 때 추가되는 점프 힘
 
     private void Start()
     {
@@ -29,83 +25,94 @@ public class CharacterMoveAbility : MonoBehaviour
     private void Update()
     {
         bool isGrounded = _characterController.isGrounded;
-        HandleMovement();
-        HandleJumpAndFall(isGrounded);
+        if (isGrounded && _yVelocity < 0)
+        {
+            _yVelocity = -0.5f; // 캐릭터가 바닥에 "붙어" 있게 함
+        }
+
+        HandleMovement(isGrounded);
+
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            StartJump(JumpPower);
+        }
+
+        ApplyGravity();
     }
 
-    void HandleMovement()
+    void HandleMovement(bool isGrounded)
     {
-        // 1. 입력받기
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        Vector3 moveDirection = Camera.main.transform.TransformDirection(h, 0, v).normalized;
 
-        // 2. 방향구하기
-        Vector3 horizontalDir = new Vector3(h, 0, v);
-        horizontalDir.Normalize();
-        horizontalDir = Camera.main.transform.TransformDirection(horizontalDir);
+        float speed = CalculateSpeed(h, v);
+        Vector3 movement = moveDirection * speed * Time.deltaTime;
+        _characterController.Move(movement);
 
-        // 3-1. 중력값 적용
-        _yVelocity += _gravity * Time.deltaTime;
-        Vector3 finalDir = new Vector3(horizontalDir.x, _yVelocity, horizontalDir.z);
-
-        float speed = MoveSpeed;
-
-        // 4. 스태미나 적용
-        if (Input.GetKey(KeyCode.LeftShift) && (h != 0 || v != 0))
-        {
-            speed = RunSpeed;
-            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 1.0f, Time.deltaTime * 3));
-        }
-
-        // 4-1. 느린 걷기 적용
-        else if (Input.GetMouseButton(0) && (h != 0 || v != 0))
-        {
-            speed = SlowSpeed;
-            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 0.33f, Time.deltaTime * 5));
-        }
-
-        else if (h != 0 || v != 0) // 일반 걷기
-        {
-            speed = MoveSpeed;
-            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 0.66f, Time.deltaTime * 4));
-        }
-        else // 정지 상태
-        {
-            _animator.SetFloat("Move", Mathf.Lerp(_animator.GetFloat("Move"), 0f, Time.deltaTime * 8));
-        }
-
-
-        // 3. 이동하기
-        _characterController.Move(finalDir * speed * Time.deltaTime);
-        //_animator.SetFloat("Move", horizontalDir.magnitude);
+        UpdateAnimator(moveDirection.magnitude, speed);
     }
 
-    void HandleJumpAndFall(bool isGrounded)
+    float CalculateSpeed(float h, float v)
     {
-        // 5. 점프 적용
-       if (isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift) && (h != 0 || v != 0))
         {
-            if (Input.GetKey(KeyCode.Space))
+            return RunSpeed;
+        }
+        if (Input.GetMouseButton(0) && (h != 0 || v != 0))
+        {
+            return SlowSpeed;
+        }
+        return MoveSpeed;
+    }
+
+    void UpdateAnimator(float magnitude, float speed)
+    {
+        // 현재 애니메이터에 설정된 'Move' 값을 가져옴
+        float currentBlend = _animator.GetFloat("Move");
+        float targetBlend;
+
+        if (magnitude > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                _yVelocity = JumpPower;
-                _animator.SetTrigger("Jump");
-                StartCoroutine(ResetJumpTrigger());
+                targetBlend = 1.0f; // 달리기
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                targetBlend = 0.33f; // 느린 걷기
             }
             else
             {
-                _yVelocity = -0.5f;
+                targetBlend = 0.66f; // 일반 걷기
             }
         }
         else
         {
-            _yVelocity = Mathf.SmoothDamp(_yVelocity, _gravity * Time.deltaTime, ref _velocitySmoothing, 0.4f, Mathf.Abs(_gravity * 1.1f));
+            targetBlend = 0f; // 정지
+        }
+
+        // Mathf.Lerp 함수를 사용하여 현재 값에서 목표 값으로 부드럽게 전환
+        _animator.SetFloat("Move", Mathf.Lerp(currentBlend, targetBlend, Time.deltaTime * 5));
+    }
+
+    void ApplyGravity()
+    {
+        _yVelocity += _gravity * Time.deltaTime;
+        _characterController.Move(new Vector3(0, _yVelocity, 0) * Time.deltaTime);
+    }
+
+    void StartJump(float power)
+    {
+        _yVelocity = power;
+        _animator.SetTrigger("Jump");
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.CompareTag("Bed") && _characterController.isGrounded)
+        {
+            StartJump(enhancedJumpPower);
         }
     }
-
-    IEnumerator ResetJumpTrigger()
-    {
-        yield return new WaitForSeconds(1f);
-        _animator.ResetTrigger("Jump");
-    }
 }
-
