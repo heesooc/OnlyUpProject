7a87d2122634b41b375719bc71b7c627baaf944d
조희수 <hs.jo.sw.p@gmail.com>
2024-04-16 12:23:14 +0900

    엘리베이터 기틀 마련

---
+++ Assets/02.Scripts/ElevatorBox.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorBox : MonoBehaviour
+{
+    // 플레이어가 탑승했을 시 위로 올라가는 코드
+    // 1층 -> 3층까지 올라가게만
+
+
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
+
+    // 문이 기본 지점에 있다가 플레이어가 앞에 다가가면 문이 이동
+    // LeftDoor는 왼쪽으로, RightDoor는 오른쪽으로 이동
+
+    
+}
